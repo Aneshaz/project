cfb8dabdd94fcc27a4fd4480d0fbf8e1df2b8903
lifang <z80645885@163.com>
2018-12-29 10:26:06 +0800

    init update-licence project

---
+++ update-licence/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <h2>补换驾照</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App"
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+</style>
