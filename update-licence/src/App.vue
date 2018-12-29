53b5a371aa88e3115e249d8b2b1c4ab1380f5d4c
lifang <z80645885@163.com>
2018-12-29 13:05:09 +0800

    add JSBridge use for webview & app  communication

--- update-licence/src/App.vue
+++ update-licence/src/App.vue
@@ -1,16 +1,26 @@
 <template>
   <div id="app">
     <h2>补换驾照</h2>
+    <button @click="login">点击登录</button>
   </div>
 </template>
 
 <script>
+const JSBridge = require("./utils/JSBridge.js");
+
 export default {
   name: "app",
   data() {
-    return {
-      msg: "Welcome to Your Vue.js App"
-    };
+    return {};
+  },
+  methods: {
+    login() {
+      JSBridge.invoke("app", "login", {
+        loginCallBackName: res => {
+          console.log("res...", res);
+        }
+      });
+    }
   }
 };
 </script>
