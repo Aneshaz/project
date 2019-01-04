a898a0d8d10cd29dc6407c0fca7600a114428f04
lifang <z80645885@163.com>
2019-01-04 19:28:04 +0800

    init chelun pc

---
+++ chelun-pc/src/pages/Index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="chelun-index">
+    <div class="header-pc">this is pc-header</div>
+    <div class="header-h5">this is h5-header</div>
+  </div>
+</template>
+<script>
+export default {
+  name: "IndexPage"
+};
+</script>
